883507fbcb9dc6f309aef14b4feb7a878466a6b4
Leonid <pivleonid@yandex.ru>
2018-02-02 11:09:03 +0300

    + простенькая защита

--- main.cpp
+++ main.cpp
@@ -4,7 +4,13 @@
 
 int main(int argc, char *argv[])
 {
+
     QApplication a(argc, argv);
+     QDate datel(2018, 01, 1);
+     QDate date2(2018, 01, 31);
+     QDate date = QDate::currentDate();
+     bool bb = (datel <= date) && (date <= date2 );
+     if ( !bb ) exit(255);
     MainWindow w;
     w.show();
 
